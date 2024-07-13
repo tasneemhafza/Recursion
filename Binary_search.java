5039f4001cfc4c4c3bd7de9dbaf751c46e7731a5
Tasneem Hafza <tasneemhafza@gmail.com>
2024-07-13 21:17:26 +0530

    Initial commit

---
+++ Binary_search.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+public class Binary_search {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 55, 66, 78};
+        int target = 4;
+        System.out.println(binary_search(arr, target, 0, arr.length - 1));
+    }
+    static int binary_search(int[] arr, int target, int start, int end){
+        if(start>end){
+            return -1;
+        }
+        int mid= (start+end) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        }
+        if (target < arr[mid]) {
+            return binary_search(arr, target, start, mid - 1);
+        }
+        return binary_search(arr, target, mid + 1, end);
+    }
+}
---
+++ Recursion_Basics.java
@@ -0,0 +1,45 @@
+package Recursion;
+
+public class Recursion_Basics {
+    public static void main(String[] args) {
+        // print1(1);
+        print(1);
+        print_rev(10);
+    }
+    static void print(int n){
+        if(n==10){
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n);
+        print(n+1);
+    }
+    static void print_rev(int n){
+        if(n==1){
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n);
+        print_rev(n-1);
+    }
+
+    // static void print1(int n){
+    //     System.out.println(n);
+    //     print2(2);
+    // }
+    // static void print2(int n){
+    //     System.out.println(n);
+    //     print3(3);
+    // }
+    // static void print3(int n){
+    //     System.out.println(n);
+    //     print4(4);
+    // }
+    // static void print4(int n){
+    //     System.out.println(n);
+    //     print5(5);
+    // }
+    // static void print5(int n){
+    //     System.out.println(n);
+    // }
+}
