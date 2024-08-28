dc33a6a4302d8cdb0f074c90c489102dc6df0e66
Tasneem Hafza <tasneemhafza@gmail.com>
2024-08-28 21:13:57 +0530

    Added more files

---
+++ MergeSort.java
@@ -0,0 +1,96 @@
+package Recursion;
+
+import java.util.Arrays;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr= {5,2,9,6,1,3};
+        // arr = mergeSort(arr);
+        // System.out.println(Arrays.toString(arr));
+
+        mergeSortInPlace(arr, 0, arr.length);
+        System.out.println(Arrays.toString(arr));
+        
+    }
+
+    static int[] mergeSort(int[] arr) {
+        if(arr.length==1) {
+            return arr;
+        }
+        int mid = arr.length/2;
+        int[] left= mergeSort(Arrays.copyOfRange(arr, 0, mid)); //mid exclusive
+        int[] right= mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+        return merge(left, right);
+    }
+
+    static int[] merge(int[] first, int[] second) {
+        int[] all= new int[first.length + second.length];
+
+        int i=0, j=0, k=0;
+        while(i<first.length && j<second.length) {
+            if(first[i] < second[j]) {
+                all[k] = first[i];
+                i++;
+            } else {
+                all[k] = second[j];
+                j++;
+            }
+            k++;
+        }
+        //when one array is completed while the other isn't
+        while(i< first.length) {
+            all[k] = first[i];
+            i++; k++;
+        }
+        while(j< second.length) {
+            all[k] = second[j];
+            j++; k++;
+        }
+        return all;
+    }
+
+    static void mergeSortInPlace(int[] arr, int s, int e) {
+        if(e-s==1) return;
+        int mid= (s+e)/2;
+        mergeSortInPlace(arr, s, mid);
+        mergeSortInPlace(arr, mid, e);
+        mergeInPlace(arr, s, mid, e);
+    }
+
+    private static void mergeInPlace(int[] arr, int s, int m, int e) {
+        int[] mix = new int[e - s];
+        int i = s;
+        int j = m;
+        int k = 0;
+
+        while (i < m && j < e) {
+            if (arr[i] < arr[j]) {
+                mix[k] = arr[i];
+                i++;
+            } else {
+                mix[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements from the left half
+        while (i < m) {
+            mix[k] = arr[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining elements from the right half
+        while (j < e) {
+            mix[k] = arr[j];
+            j++;
+            k++;
+        }
+
+        // Copy the sorted elements back into the original array
+        for (int l = 0; l < mix.length; l++) {
+            arr[s + l] = mix[l];
+        }
+    }
+}
---
+++ Skipping.java
@@ -0,0 +1,65 @@
+package Recursion;
+
+public class Skipping {
+    public static void main(String[] args) {
+        skip1("", "banana");
+        System.out.println(skip2("apple"));
+        System.out.println(skip3("baappleappcat"));
+        System.out.println(skip4("baapplappcat"));
+    }
+
+    static void skip1(String p, String up) {
+        if(up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+        char ch= up.charAt(0);
+        if(ch=='a') {
+            skip1(p, up.substring(1));
+        }
+        else {
+            skip1(p+ch, up.substring(1));
+        }
+    }
+// without updating or passing p string
+    static String skip2(String up) {
+        if(up.isEmpty()) {
+            return "";
+        }
+        char ch= up.charAt(0);
+        if(ch=='a') {
+            return skip2(up.substring(1));
+        }
+        else {
+            return ch + skip2(up.substring(1));
+        }
+    } 
+
+// skip apple 
+    static String skip3(String up) {
+        if(up.isEmpty()) {
+            return "";
+        }
+        if(up.startsWith("apple")) {
+            return skip3(up.substring(5));
+        }
+        else {
+            return up.charAt(0) + skip3(up.substring(1));
+        }
+    } 
+
+// skip app if its not apple
+    static String skip4(String up) {
+        if(up.isEmpty()) {
+            return "";
+        }
+        if(up.startsWith("app") && !up.startsWith("apple")) {
+            return skip4(up.substring(3));
+        }
+        else {
+            return up.charAt(0) + skip4(up.substring(1));
+        }
+    } 
+
+
+}
